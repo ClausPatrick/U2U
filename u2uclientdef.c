b9113fa5068775d6327583bcbf6dfb234e506e9a
ClausPatrick <44665589+ClausPatrick@users.noreply.github.com>
2023-09-01 16:05:06 +0100

    Create u2uclientdef.c

---
+++ u2uclientdef.c
@@ -0,0 +1,9 @@
+//u2uclientdef.c
+
+#include "u2uclientdef.h"
+
+
+
+const char* SELF_IP_ADDRESS =  "192.168.1.111";
+const char PEER_IP_LIST[PEER_COUNT][MAX_IP_LEN] = {"192.168.1.111", "192.168.1.222", "192.168.1.333"};
+const char PEER_NAME_LIST[PEER_COUNT][32] = {"NAME1", "NAME2", "NAME3"};
