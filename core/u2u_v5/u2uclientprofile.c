197dd1ac8e7268cad5e71b29e9d1c5d1490b7ed7
ClausPatrick <44665589+ClausPatrick@users.noreply.github.com>
2026-04-29 10:05:42 +0100

    Create u2uclientprofile.c

---
+++ core/u2u_v5/u2uclientprofile.c
@@ -0,0 +1,23 @@
+//u2uclientprofile.c
+#include "u2u_client_profile.h"
+
+
+const char* topic_default_responses[] = {
+                "Response from topic: 'HAIL____'. ",
+                "Response from topic: 'HELP____'. ",
+                "Response from topic: 'SET_LCD_'. ",
+                "Response from topic: 'SET_OLED'. ",
+                "Response from topic: 'GET_SNSR'. ",
+                "Response from topic: 'SET_ENCR'. ",
+                "Response from topic: 'GET_ENCR'. ",
+                "Response from topic: 'SET_LED_'. ",
+                "Response from topic: 'SET_TIME'. ",
+                "Response from topic: 'GET_TIME'. ",
+                "Response from topic: 'SET_DATE'. ",
+                "Response from topic: 'GET_DATE'. ",
+                "Response from topic: 'RSERVD_0'. ",
+                "Response from topic: 'RSERVD_1'. ",
+                "Response from topic: 'RSERVD_2'. ",
+                "Response from topic: 'RSERVD_3'. ",
+                "Response from topic: 'RSERVD_4'. ",
+                "Unrecognised topic: '"};
